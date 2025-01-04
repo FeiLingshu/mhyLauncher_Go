c6652017c3b899e1afc796c40e69fb83d50f962a
FeiLingshu <93750136+FeiLingshu@users.noreply.github.com>
2025-01-04 14:58:22 +0800

    Add files via upload

---
+++ mhyLauncher_Go/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("mhyLauncher_Go")]
+[assembly: AssemblyDescription("对米哈游启动器支持的部分游戏进行窗口化调整的工具程序")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("(无)")]
+[assembly: AssemblyProduct("mhyLauncher_Go")]
+[assembly: AssemblyCopyright("Copyright 2024 By FeiLingshu")]
+[assembly: AssemblyTrademark("FeiLingshu")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("1B1FBA27-60A8-4F8B-B107-73EA7DA0CE56")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
+//通过使用 "*"，如下所示:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1.1")]
+[assembly: NeutralResourcesLanguage("zh-Hans")]
